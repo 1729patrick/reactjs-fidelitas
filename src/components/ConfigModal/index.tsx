ea2dee619c64075157a41fdaefa4cd05c2967e0c
Tomás <tommasini15@gmail.com>
2021-07-10 01:19:34 +0100

    config modal for reserves view done

--- src/components/ConfigModal/index.tsx
+++ src/components/ConfigModal/index.tsx
@@ -2,26 +2,26 @@ import React, { useEffect, useState } from 'react';
 import Modal from '@material-ui/core/Modal';
 import {
   Button,
+  Checkbox,
   FormControl,
+  FormControlLabel,
+  FormGroup,
   IconButton,
   Input,
   InputAdornment,
   InputLabel,
+  Switch,
 } from '@material-ui/core';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
-import clsx from 'clsx';
-import {
-  KeyboardDatePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
+
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     paper: {
       position: 'absolute',
-      width: 400,
+      width: 600,
       backgroundColor: theme.palette.background.paper,
       border: '2px solid #000',
       boxShadow: theme.shadows[5],
@@ -50,33 +50,21 @@ type Props = {
 };
 
 const ConfigModal: React.FC<Props> = ({ handleCloseModal }) => {
+  const [acceptReserves, setAcceptReserves] = useState(false);
+  const [weekDays, setWeekDays] = useState([
+    { weekDay: 'segunda-feira', selected: true },
+    { weekDay: 'terça-feira', selected: true },
+    { weekDay: 'quarta-feira', selected: true },
+    { weekDay: 'quinta-feira', selected: true },
+    { weekDay: 'sexta-feira', selected: true },
+    { weekDay: 'sábado', selected: true },
+    { weekDay: 'domingo', selected: true },
+  ]);
   const classes = useStyles();
 
-  /* const [formControl, setFormControl] = useState<{ [key: string]: any }>(
-    dataRef.current,
-  );*/
-  const [selectedDate, setSelectedDate] = useState<Date | null>();
-  /**
-   * TO handle the data
-   */
-  /* useEffect(() => {
-    console.log(dataRef);
-    if (dataRef) {
-      setSelectedDate(dataRef.current.expirationDate);
-    }
-  }, []);*/
-
-  const handleChangeDate = (date: Date | null) => {
-    setSelectedDate(date);
+  const handleAcceptReserve = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAcceptReserves(!acceptReserves);
   };
-
-  /*const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
-    setFormControl({
-      ...formControl,
-      [event.target.name]: event.target.value,
-    });
-  };*/
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -101,35 +89,155 @@ const ConfigModal: React.FC<Props> = ({ handleCloseModal }) => {
             color="inherit"
             aria-label="open drawer"
             edge="start"
-            //onClick={handleCloseModal}
-          >
+            onClick={handleCloseModal}>
             <HighlightOffIcon fontSize={'large'} />
           </IconButton>
         </div>
+        <FormControlLabel
+          value="top"
+          control={<Switch color="primary" />}
+          label="Aceita reservas"
+          labelPlacement="top"
+        />
         <form onSubmit={handleSubmit}>
-          {/*<FormControl
-                className={clsx(classes.margin, classes.textField)}
-            >
-                <InputLabel htmlFor={field.id}>{field.label}</InputLabel>
-                <Input
-                    id={field.id}
-                    type={field.type}
-                    value={formControl[field.id]}
-                    name={field.id}
-                    onChange={handleChange}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <FormControl className={clsx(classes.margin, classes.textField)}>
+              <InputLabel htmlFor={'minPersons'}>
+                {'Nº mínimo de pessoas'}
+              </InputLabel>
+              <Input
+                id={'minPersons'}
+                type={'number'}
+                // value={''}
+                name={'min'}
+                onChange={() => {}}
+                endAdornment={
+                  <InputAdornment position="end">
+                    {/*<IconButton
                               aria-label="toggle password visibility"
                               onClick={handleClickShowPassword}
                               onMouseDown={handleMouseDownPassword}>
                               {showPassword ? <Visibility/> : <VisibilityOff/>}
-                          </IconButton>
-                        </InputAdornment>
-                    }
-                />
-            </FormControl>*/}
+                          </IconButton> */}
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
 
+            <FormControl className={clsx(classes.margin, classes.textField)}>
+              <InputLabel htmlFor={'maxPersons'}>
+                {'Nº máximo de pessoas'}
+              </InputLabel>
+              <Input
+                id={'maxPersons'}
+                type={'number'}
+                // value={''}
+                name={'max'}
+                onChange={() => {}}
+                endAdornment={
+                  <InputAdornment position="end">
+                    {/*<IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}>
+                              {showPassword ? <Visibility/> : <VisibilityOff/>}
+                          </IconButton> */}
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <FormControl className={clsx(classes.margin, classes.textField)}>
+              <InputLabel htmlFor={'limitTimeReservation'}>
+                {'Hora limite de reserva'}
+              </InputLabel>
+              <Input
+                id={'maxPersons'}
+                type={'number'}
+                // value={''}
+                name={'max'}
+                onChange={() => {}}
+                endAdornment={
+                  <InputAdornment position="end">
+                    {/*<IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}>
+                              {showPassword ? <Visibility/> : <VisibilityOff/>}
+                          </IconButton> */}
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl className={clsx(classes.margin, classes.textField)}>
+              <InputLabel htmlFor={'late'}>
+                {'Tempo de atraso permitido'}
+              </InputLabel>
+              <Input
+                id={'maxPersons'}
+                type={'number'}
+                // value={''}
+                name={'max'}
+                onChange={() => {}}
+                endAdornment={
+                  <InputAdornment position="end">
+                    {/*<IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}>
+                              {showPassword ? <Visibility/> : <VisibilityOff/>}
+                          </IconButton> */}
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </div>
+          <FormControl className={clsx(classes.margin, classes.textField)}>
+            <InputLabel htmlFor={'signal'}>{'Valor do sinal'}</InputLabel>
+            <Input
+              id={'maxPersons'}
+              type={'number'}
+              // value={''}
+              name={'max'}
+              onChange={() => {}}
+              endAdornment={
+                <InputAdornment position="end">
+                  {/*<IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}>
+                              {showPassword ? <Visibility/> : <VisibilityOff/>}
+                          </IconButton> */}
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <h3>Dia da semana que aceita reservas</h3>
+          <FormGroup row>
+            {weekDays.map((weekDay, index) => (
+              <FormControlLabel
+                key={index}
+                control={
+                  <Checkbox
+                    checked={weekDay.selected}
+                    onChange={() => {
+                      let wekDays = weekDays;
+                      wekDays.map((w, i) => {
+                        if (index === i) {
+                          w.selected = !w.selected;
+                        }
+                      });
+                      setWeekDays([...wekDays]);
+                    }}
+                    name={weekDay.weekDay}
+                    color={'primary'}
+                  />
+                }
+                label={weekDay.weekDay}
+              />
+            ))}
+          </FormGroup>
           <div
             style={{
               display: 'flex',
